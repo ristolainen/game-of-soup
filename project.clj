654cdcd6c12c9994e6591b141d6fa8f60ae5d6bf
Kristoffer Johansson <kristoffer.s.johansson@gmail.com>
2013-07-03 19:33:13 +0100

    Editet project.xml and added dependencies

--- project.clj
+++ project.clj
@@ -1,6 +1,8 @@
 (defproject soup "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
+  :description "The Game Of Soup"
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.5.1"]])
+  :dependencies [[org.clojure/clojure "1.5.1"]
+				 [clojureql "1.0.0"]
+				 [mysql/mysql-connector-java "5.1.25"]])
