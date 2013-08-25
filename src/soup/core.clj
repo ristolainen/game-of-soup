bb0f7faee5366b55c92db438409f9fb97827ef4b
Kristoffer Johansson <kristoffer.s.johansson@gmail.com>
2013-08-25 18:55:38 +0200

    logic for applying actions to plates

--- src/soup/core.clj
+++ src/soup/core.clj
@@ -3,3 +3,53 @@
 
 (defn initialize []
   (db/load-prod-db))
+
+(defrecord Plate [table-pos poison glass antidote])
+
+(defrecord Action [type action-data])
+
+(defrecord Player [id user table-pos])
+
+(defn create-plate [table-pos]
+  (Plate. table-pos false 0 false))
+
+(defn index-plates [plates]
+  (into {} (map (fn [[k v]] [k (first v)]) 
+                (group-by :table-pos plates))))
+
+(defn add-poison [plate]
+  (assoc plate :poison true))
+
+(defn add-antidote [plate]
+  (assoc plate :antidote true))
+
+(defn add-glass [plate]
+  (update-in plate [:glass] inc))
+
+(defn switch-plates [plates from to]
+  (assoc plates from (plates to) to (plates from)))
+
+(defmulti act :type)
+
+(defmethod act :poison [action plates]
+  (let [pos (-> action :action-data :pos)
+        plate (plates pos)]
+    (assoc plates pos (add-poison plate))))
+
+(defmethod act :antidote [action plates]
+  (let [pos (-> action :action-data :pos)
+        plate (plates pos)]
+    (assoc plates pos (add-antidote plate))))
+
+(defmethod act :glass [action plates]
+  (let [pos (-> action :action-data :pos)
+        plate (plates pos)]
+    (assoc plates pos (add-glass plate))))
+
+(defmethod act :switch [action plates]
+  (let [{from :from to :to} (:action-data action)]
+    (switch-plates plates from to)))
+
+(defn apply-action [action plates]
+  (let [indexed-plates (index-plates plates)]
+    (act action indexed-plates)))
--- test/soup/core_test.clj
+++ test/soup/core_test.clj
@@ -1,5 +1,29 @@
 (ns soup.core-test
   (:require [clojure.test :refer :all]
-            [soup.core :refer :all]))
+            [soup.core :refer :all])
+  (:import [soup.core Action Plate]))
+
+(deftest test-apply-action
+  (testing "poison"
+    (let [action (Action. :poison {:pos 1})
+          plates [(create-plate 1)]
+          result (apply-action action plates)]
+      (is (= 1 (count result)))
+      (is (:poison (result 1)))))
+  (testing "antidote"
+    (let [action (Action. :antidote {:pos 2})
+          plates [(create-plate 1) (assoc (create-plate 2) :poison true)]
+          result (apply-action action plates)]
+      (is (= 2 (count result)))
+      (is (:antidote (result 2)))
+      (is (:poison (result 2)))))
+  (testing "switch"
+    (let [action (Action. :switch {:from 1 :to 3})
+          plates [(assoc (create-plate 1) :id :a) (assoc (create-plate 2) :id :b) (assoc (create-plate 3) :id :c)]
+          result (apply-action action plates)]
+      (is (= 3 (count result)))
+      (is (= :c (:id (result 1))))
+      (is (= :b (:id (result 2))))
+      (is (= :a (:id (result 3)))))))
 
 
